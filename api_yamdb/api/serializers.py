a899edef6595c1c05061768407a310ab62481c9a
Сергей Стребков <sergei-strebkov@mail.ru>
2023-03-31 12:43:59 +0300

    Added Title and fixed bugs

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
-from reviews.models import Genre, Category
+from reviews.models import Genre, Category, Title
 
 
 class GenreSerializer(serializers.ModelSerializer):
+    """Сериализатор жанров."""
     class Meta:
         model = Genre
         lookup_field = 'slug'
@@ -10,7 +11,36 @@ class GenreSerializer(serializers.ModelSerializer):
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    """Сериализатор категорий."""
     class Meta:
         model = Category
         lookup_field = 'slug'
         exclude = ('id', )
+
+
+class TitleReadOnlySerializer(serializers.ModelSerializer):
+    """Сериализатор для чтения произведений."""
+    category = CategorySerializer(read_only=True)
+    genre = GenreSerializer(many=True,
+                            read_only=True)
+
+    class Meta:
+        model = Title
+        fields = '__all__'
+
+
+class TitleWriteSerializer(serializers.ModelSerializer):
+    """Сериализатор для записи произведений."""
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        slug_field='slug',
+        many=True
+    )
+
+    class Meta:
+        model = Title
+        fields = '__all__'
--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -2,13 +2,15 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import (GenreViewSet,
-                    CategoryViewSet)
+                    CategoryViewSet,
+                    TitleViewSet)
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register(r'genres', GenreViewSet, basename='genres')
 router.register(r'categories', CategoryViewSet, basename='categories')
+router.register(r'titles', TitleViewSet, basename='titles')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
